c330027c3ddd3990b255e0e87334bfe1c4ff3260
rmulcahy1 <73314478+rmulcahy1@users.noreply.github.com>
2021-05-07 00:59:12 -0400

    Update dark_and_stormy.R

--- dark_and_stormy.R
+++ dark_and_stormy.R
@@ -1,1 +1,1 @@
-print("it was a dark and stormy night so I stayed in to complete my R project. (Helped me avoid Covid-19 too.)")
+print("it was a dark and stormy night so I stayed in to complete my R project. (Helped me avoid Covid-19 too.) ")
